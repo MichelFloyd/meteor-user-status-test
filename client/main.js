b6c43c0d1f66f2ea201d659de23f3dedf2435e6f
MichelFloyd <michel.floyd@cloaklabs.com>
2017-11-09 11:38:29 -0800

    changes to test ip address fetching

--- client/main.js
+++ client/main.js
@@ -6,13 +6,33 @@ import './main.html';
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
+  Meteor.subscribe('connections');
+  Meteor.subscribe('me');
 });
 
 Template.hello.helpers({
   counter() {
     return Template.instance().counter.get();
-  },
+  }
 });
+
+Template.connections.onCreated(function(){
+  const self = this;
+  self.ipAddr = new ReactiveVar('');
+  Meteor.call('getIpAddress',(err,result) => {
+    if (!err) self.ipAddr.set(result);
+  });
+});
+
+Template.connections.helpers({
+  connections() {
+    return Connections.find();
+  },
+  myIp() {
+    return Template.instance().ipAddr.get();
+    //return Connections.findOne({userId: Meteor.userId},{sort: {loginTime: -1}, limit: 1}).ipAddr;
+  }
+})
 
 Template.hello.events({
   'click button'(event, instance) {
--- server/main.js
+++ server/main.js
@@ -1,7 +1,50 @@
+import { Accounts } from 'meteor/accounts-base';
 import {UserStatus} from 'meteor/mizzao:user-status';
 
-setInterval(() => {
-  UserStatus.connections.find().forEach((c) => {
-    console.log("got", c);
+Meteor.publish('connections',()=>{
+  return Connections.find();
+});
+
+Meteor.publish('me',function(){
+  return Meteor.users.find(this.userId);
+});
+
+Meteor.startup(() => {
+  if (!Meteor.users.findOne()){
+    Accounts.createUser({email: "admin@admin.com", password: "password"});
+  }
+  Meteor.setInterval(()=>{    
+    UserStatus.connections.find().forEach((c) => {
+      console.log("got", c);
+      Connections.upsert({_id: c._id},{$set: c});
+    });
+  },10000);
+});
+
+let ipAddr;
+
+/*
+Meteor.onConnection((connection) => {
+  ipAddr = connection.clientAddress;
+});
+
+Meteor.methods({
+  getIpAddress() {
+    return ipAddr;
+  }
+});
+*/
+
+Accounts.onLogin(() => {
+  Meteor.call('clearImpersonation', (err, result) => {
+    if (err) console.log('Error clearing impersonation: ',err);
   });
-}, 1000);
+});
+
+Meteor.methods({
+  clearImpersonation(){
+    const user = Meteor.users.findOne(this.userId);
+    if (user && user.impersonating) Meteor.users.update(user._id,{ $unset: 'profile.impersonating' });
+    return;
+  }
+});
